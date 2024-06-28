657f74a16208a2dac1eba3409ca0c5edff8b7b86
Salif Saidou <104588234+SAIDOU11@users.noreply.github.com>
2024-06-28 08:47:29 +0200

    props basics

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,34 @@
 import "./index.css";
 import Greet from "./components/Greet";
+import Person from "./components/Person";
+import PersonList from "./components/PersonList";
 
 function App() {
+  const personName = {
+    first: "Bruce",
+    last: "Wayne",
+  };
+
+  const nameList = [
+    {
+      first: "Walter",
+      last: "White",
+    },
+    {
+      first: "Saul",
+      last: "Goodman",
+    },
+    {
+      first: "Gustavo",
+      last: "Fring",
+    },
+  ];
+
   return (
     <>
-      <Greet name="Bob" />
+      <Greet name="Bob" messageCount={10} isLoggedIn={true} />
+      <Person name={personName} />
+      <PersonList names={nameList} />
     </>
   );
 }
---
+++ src/components/PersonList.tsx
@@ -0,0 +1,23 @@
+type PersonListProps = {
+  names: {
+    first: string;
+    last: string;
+  }[];
+};
+
+const PersonList = (props: PersonListProps) => {
+  return (
+    <div>
+      {props.names.map((name) => {
+        return (
+          <h2 key={name.first} className="m-4 text-[20px] text-blue-600">
+            {" "}
+            {name.first} {name.last}{" "}
+          </h2>
+        );
+      })}
+    </div>
+  );
+};
+
+export default PersonList;
