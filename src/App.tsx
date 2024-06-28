a836806aa15fb62ec992534055effdd009db512e
Salif Saidou <104588234+SAIDOU11@users.noreply.github.com>
2024-06-28 08:49:18 +0200

    start over

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,9 @@
 import "./index.css";
-import Greet from "./components/Greet";
-import Person from "./components/Person";
-import PersonList from "./components/PersonList";
 
 function App() {
-  const personName = {
-    first: "Bruce",
-    last: "Wayne",
-  };
-
-  const nameList = [
-    {
-      first: "Walter",
-      last: "White",
-    },
-    {
-      first: "Saul",
-      last: "Goodman",
-    },
-    {
-      first: "Gustavo",
-      last: "Fring",
-    },
-  ];
-
   return (
     <>
-      <Greet name="Bob" messageCount={10} isLoggedIn={true} />
-      <Person name={personName} />
-      <PersonList names={nameList} />
+      <h1 className="text-4xl text-blue-800 m-4">Welcome Back</h1>
     </>
   );
 }
